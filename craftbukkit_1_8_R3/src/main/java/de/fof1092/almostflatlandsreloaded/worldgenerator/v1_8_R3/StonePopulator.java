cd0b0c56f296bb9e156d49fe1f36c06d8a7f9e34
MotikaCraft-TheJoshua <56804505+MotikaCraft-TheJoshua@users.noreply.github.com>
2025-06-14 20:12:31 +0200

    Update StonePopulator.java

--- craftbukkit_1_8_R3/src/main/java/de/fof1092/almostflatlandsreloaded/worldgenerator/v1_8_R3/StonePopulator.java
+++ craftbukkit_1_8_R3/src/main/java/de/fof1092/almostflatlandsreloaded/worldgenerator/v1_8_R3/StonePopulator.java
@@ -32,12 +32,10 @@ final class StonePopulator {
 	 */
 	static ChunkData populate(int x, int y, int z, ChunkData cd, Random random, List<String> worldUndergroundMaterialsNames) {
 		int startY = Options.worldDepth + (Options.flatBedrockEnabled ? Options.flatBedrockThickness : 1);
-		Bukkit.getLogger().info("[AlmostFlatLandsReloaded] StonePopulator: x=" + x + ", z=" + z + ", startY=" + startY + ", flatBedrockEnabled=" + Options.flatBedrockEnabled);
 		int randomBlockType = random.nextInt(worldUndergroundMaterialsNames.size());
 
 		for (int newY = startY; newY < y; newY++) {
 			cd.setBlock(x, newY, z, Options.worldUndergroundMaterials.get(randomBlockType));
-			Bukkit.getLogger().info("[AlmostFlatLandsReloaded] StonePopulator: Set block at y=" + newY + ", material=" + Options.worldUndergroundMaterials.get(randomBlockType));
 		}
 
 		return cd;
