d276cec81525eb08a9782aa1730bc883dc336d22
MotikaCraft-TheJoshua <56804505+MotikaCraft-TheJoshua@users.noreply.github.com>
2025-06-14 20:22:16 +0200

    Update WorldGenerator.java

--- craftbukkit_1_8_R3/src/main/java/de/fof1092/almostflatlandsreloaded/worldgenerator/v1_8_R3/WorldGenerator.java
+++ craftbukkit_1_8_R3/src/main/java/de/fof1092/almostflatlandsreloaded/worldgenerator/v1_8_R3/WorldGenerator.java
@@ -1,12 +1,10 @@
 package de.fof1092.almostflatlandsreloaded.worldgenerator.v1_8_R3;
 
 import de.fof1092.almostflatlandsreloaded.Options;
-import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.generator.BlockPopulator;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.util.noise.SimplexOctaveGenerator;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -15,21 +13,17 @@ import java.util.Random;
  * WorldGenerator manages the AFLR world generator for Minecraft 1.8.
  */
 public class WorldGenerator extends ChunkGenerator {
-
     @Override
     public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biomeGrid) {
         ChunkData cd = createChunkData(world);
         SimplexOctaveGenerator wgen = new SimplexOctaveGenerator(world, 8);
         wgen.setScale(0.015625D);
-        Bukkit.getLogger().info("[AlmostFlatLandsReloaded] Generating chunk: " + chunkX + ", " + chunkZ);
 
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
                 int realX = x + chunkX * 16;
                 int realZ = z + chunkZ * 16;
-
                 double normalHeight = (wgen.noise(realX, realZ, 0.5D, 0.5D) / 0.75) + Options.worldHeight;
-                Bukkit.getLogger().info("[AlmostFlatLandsReloaded] WorldGenerator: x=" + x + ", z=" + z + ", height=" + (int) normalHeight);
 
                 cd = BedrockPopulator.populate(x, z, cd, random);
                 cd = StonePopulator.populate(x, (int) normalHeight, z, cd, random, Options.worldUndergroundMaterialNames);
@@ -37,7 +31,6 @@ public class WorldGenerator extends ChunkGenerator {
                     cd = OrePopulator.populate(x, (int) normalHeight, z, cd, random);
                 }
                 cd = GroundPopulator.populate(x, (int) normalHeight, z, cd, random);
-
                 biomeGrid.setBiome(x, z, Options.worldBiome);
             }
         }
@@ -51,7 +44,6 @@ public class WorldGenerator extends ChunkGenerator {
         populators.add(new DataValuePopulator());
         populators.add(new FlowerAndGrassPopulator());
         populators.add(new TreePopulator());
-
         return populators;
     }
 }
