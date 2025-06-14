c4ff40e9ca930c075639e24c12f0725e99839f9d
MotikaCraft-TheJoshua <56804505+MotikaCraft-TheJoshua@users.noreply.github.com>
2025-06-14 16:26:28 +0200

    Update OrePopulator.java

--- craftbukkit_1_18_R1_AND_ABOVE/src/main/java/de/fof1092/almostflatlandsreloaded/worldgenerator/v1_18_R1_AND_ABOVE/OrePopulator.java
+++ craftbukkit_1_18_R1_AND_ABOVE/src/main/java/de/fof1092/almostflatlandsreloaded/worldgenerator/v1_18_R1_AND_ABOVE/OrePopulator.java
@@ -26,38 +26,63 @@ final class OrePopulator {
 	 * @param z the z position within the chunk
 	 * @param cd the current ChunkData of the chunk
 	 * @param random the randomizer of the world
-	 *
 	 * @return the new ChunkData of the chunk
 	 */
 	static ChunkData populate(int x, int y, int z, ChunkData cd, Random random) {
-		for (int i = 0; i < 20*10; i++) {
-			setRandomBlock(x, y, z, cd, random, 128, Material.COAL_ORE);
-		}
-		
-		for (int i = 0; i < 20*6; i++) {
-			setRandomBlock(x, y, z, cd, random, 64, Material.IRON_ORE);
-		}
-		
-		for (int i = 0; i < 1*4; i++) {
-			setRandomBlock(x, y, z, cd, random, 32, Material.GOLD_ORE);
-		}
-		
-		for (int i = 0; i < 4*1; i++) {
-			setRandomBlock(x, y, z, cd, random, 32, Material.EMERALD_ORE);
-		}
-		
-		for (int i = 0; i < 8*6; i++) {
-			setRandomBlock(x, y, z, cd, random, 16, Material.REDSTONE_ORE);
-		}
-		
-		for (int i = 0; i < 1*7; i++) {
-			setRandomBlock(x, y, z, cd, random, 16, Material.DIAMOND_ORE);
+		for (int i = 0; i < Options.worldOres.size(); i++) {
+			Material material = Options.worldOres.get(i);
+			int chance = Options.worldOreChances.get(i);
+			int maxHeight;
+			switch (material.toString()) {
+				case "COAL_ORE":
+					maxHeight = 128;
+					break;
+				case "IRON_ORE":
+					maxHeight = 64;
+					break;
+				case "GOLD_ORE":
+				case "EMERALD_ORE":
+					maxHeight = 32;
+					break;
+				case "REDSTONE_ORE":
+				case "DIAMOND_ORE":
+				case "LAPIS_ORE":
+					maxHeight = 16;
+					break;
+				default:
+					maxHeight = 64; // Default for custom materials
+			}
+			int attempts;
+			switch (material.toString()) {
+				case "COAL_ORE":
+					attempts = 20 * 10;
+					break;
+				case "IRON_ORE":
+					attempts = 20 * 6;
+					break;
+				case "GOLD_ORE":
+					attempts = 1 * 4;
+					break;
+				case "EMERALD_ORE":
+					attempts = 4 * 1;
+					break;
+				case "REDSTONE_ORE":
+					attempts = 8 * 6;
+					break;
+				case "DIAMOND_ORE":
+					attempts = 1 * 7;
+					break;
+				case "LAPIS_ORE":
+					attempts = 1 * 5;
+					break;
+				default:
+					attempts = 10; // Default for custom materials
+			}
+			for (int j = 0; j < attempts; j++) {
+				setRandomBlock(x, y, z, cd, random, maxHeight, material, chance);
+			}
 		}
 
-		for (int i = 0; i < 1*5; i++) {
-			setRandomBlock(x, y, z, cd, random, 16, Material.LAPIS_ORE);
-		}
-		
 		return cd;
 	}
 
@@ -71,14 +96,15 @@ final class OrePopulator {
 	 * @param random the randomizer of the WorldGenerator
 	 * @param maxHeight the maximum y of the block
 	 * @param material the material of the block to be placed
+	 * @param chance the spawn chance (1–100)
 	 */
-	private static void setRandomBlock(int x, int y, int z, ChunkData cd, Random random, int maxHeight, Material material) {
-		int chance = random.nextInt(100) + 1;
-		if (chance <= Options.worldOresChance) {
-
+	private static void setRandomBlock(int x, int y, int z, ChunkData cd, Random random, int maxHeight, Material material, int chance) {
+		int roll = random.nextInt(100) + 1;
+		if (roll <= chance) {
 			int rndX = random.nextInt(16);
 			int rndZ = random.nextInt(16);
-			int rndY = random.nextInt((maxHeight - Options.getWorldDepth) - 4) + 4 + Options.getWorldDepth;
+			int startY = Options.worldDepth + (Options.flatBedrockEnabled ? Options.flatBedrockThickness : 1);
+			int rndY = random.nextInt((maxHeight - startY) - 4) + 4 + startY;
 
 			if (rndX == x && rndZ == z) {
 				if (rndY <= y) {
@@ -87,5 +113,4 @@ final class OrePopulator {
 			}
 		}
 	}
-
 }
