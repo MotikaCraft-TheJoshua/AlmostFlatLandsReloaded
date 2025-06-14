e7376888ece6ebd947f45f305c28f1e795039f9d
MotikaCraft-TheJoshua <56804505+MotikaCraft-TheJoshua@users.noreply.github.com>
2025-06-14 14:28:51 +0200

    Update WorldGenerator.java

--- craftbukkit_1_8_R3/src/main/java/de/fof1092/almostflatlandsreloaded/worldgenerator/v1_8_R3/WorldGenerator.java
+++ craftbukkit_1_8_R3/src/main/java/de/fof1092/almostflatlandsreloaded/worldgenerator/v1_8_R3/WorldGenerator.java
@@ -20,34 +20,30 @@ public class WorldGenerator extends ChunkGenerator {
 	 */
 	@Override
 	public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biomeGrid) {
-		
 		ChunkData cd = createChunkData(world);
 
 		SimplexOctaveGenerator wgen = new SimplexOctaveGenerator(world, 8);
 		wgen.setScale(0.015625D);
-		
+
 		for (int x = 0; x < 16; x++) {
 			for (int z = 0; z < 16; z++) {
-
 				int realX = x + chunkX * 16;
 				int realZ = z + chunkZ * 16;
-		        
-		        double normalHight = (wgen.noise(realX, realZ, 0.5D, 0.5D) / 0.75) + Options.worldHeight;
-		        
-		        
-		        cd = StonePopulator.populate(x, (int) normalHight, z, cd, random);
-		        
-		        if (Options.worldOresChance > 0) {
-		        	cd = OrePopulator.populate(x, (int) normalHight, z, cd, random);
-		        }
-		        
+
+		  	      double normalHeight = (wgen.noise(realX, realZ, 0.5D, 0.5D) / 0.75) + Options.worldHeight;
+
+				// Pass original material names to StonePopulator
+				cd = StonePopulator.populate(x, (int) normalHeight, z, cd, random, Options.worldUndergroundMaterialNames);
+				if (Options.worldOresChance > 0) {
+					cd = OrePopulator.populate(x, (int) normalHeight, z, cd, random);
+				}
 				cd = BeadrockPopulator.populate(x, z, cd, random);
-				cd = GroundPopulator.populate(x, (int) normalHight, z, cd, random);
-		        
+				cd = GroundPopulator.populate(x, (int) normalHeight, z, cd, random);
+
 				biomeGrid.setBiome(x, z, Options.worldBiome);
 			}
 		}
-		
+
 		return cd;
 	}
 
@@ -56,11 +52,11 @@ public class WorldGenerator extends ChunkGenerator {
 	 */
 	@Override
 	public List<BlockPopulator> getDefaultPopulators(World world) {
-	    List<BlockPopulator> populators = new ArrayList<>();
-	    populators.add(new FlowerAndGrassPopulator());
-	    populators.add(new TreePopulator());
+		List<BlockPopulator> populators = new ArrayList<>();
+		populators.add(new FlowerAndGrassPopulator());
+		populators.add(new TreePopulator());
 	    
-	    return populators;
+		return populators;
 	}
-	
+
 }
